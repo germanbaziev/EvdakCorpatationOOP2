8d6323558229f2ccb86ad9c609ec91f7cc9c9d59
germanbaziev <128309083+germanbaziev@users.noreply.github.com>
2023-06-09 22:37:24 +0300

    deleated useless method

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -32,37 +32,6 @@ void MainWindow::on_pushButton_clicked()
 
 }
 
-
-//bool balanced(const std::string &s) {
-//    std::stack<char> stack;
-//    for (char c : s) {
-//        switch (c) {
-
-//        case '(': stack.push(')'); break;
-//        case '[': stack.push(']'); break;
-//        case '{': stack.push('}'); break;
-//        case '<': stack.push('>'); break;
-
-//        case ')':
-//        case ']':
-//        case '}':
-//        case '>':
-//            if (stack.empty() || stack.top() != c) {
-//                return false;
-//            }
-//            stack.pop();
-//            break;
-//        default:
-//            break;
-//        }
-//    }
-//    return stack.empty();
-//}
-
-
-
-
-
 void MainWindow::on_pushButton_2_clicked()
 {
     JsonChecker checker(textFile);
